301402147cf1dd4f073ef96c53e4af3fbbeadef5
seohyunlinalim <seo_hyun_lim@brown.edu>
2023-12-04 18:03:37 -0500

    Cut: example lina operator

--- email_operator.py
+++ email_operator.py
@@ -5,8 +5,8 @@ from datetime import datetime, timedelta
 from airflow.operators.python_operator import PythonOperator 
 from airflow.operators.email_operator import EmailOperator
 
-default_args = { 
-    'owner': 'airflow', 
+# default_args = { 
+#     'owner': 'airflow', 
     #'start_date': airflow.utils.dates.days_ago(2), 
     # 'end_date': datetime(), 
     # 'depends_on_past': False, 
@@ -16,11 +16,11 @@ default_args = {
     # If a task fails, retry it once after waiting 
     # at least 5 minutes 
     #'retries': 1, 'retry_delay': timedelta(minutes=5), 
-    }
+    # }
 
 dag_email = DAG( 
     dag_id = 'emailoperator_demo', 
-    default_args=default_args, 
+    # default_args=default_args, 
     schedule_interval='@once', 
     dagrun_timeout=timedelta(minutes=60), 
     description='use case of email operator in airflow', 
@@ -37,8 +37,8 @@ start_task = PythonOperator(
 send_email = EmailOperator( 
     task_id='send_email', 
     to='seohyunlim98@gmail.com', 
-    subject='ingestion complete', 
-    html_content="Date: {{ ds }}", 
+    subject='Alert Mail', 
+    html_content=""" Mail Test """, 
     dag=dag_email)
     
 send_email.set_upstream(start_task) 
